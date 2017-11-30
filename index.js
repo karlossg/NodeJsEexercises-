4ec798f679158afe05143915f9b0f528696695c5
Karol <karol.gratus@gmail.com>
2017-11-30 14:36:35 +0100

    read dir

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
-const EventEmitter = require("events").EventEmitter;
-const OSinfo = require('./modules/OSinfo');
+const fs = require('fs');
+const colors = require('colors');
+// const StatMode = require('stat-mode');
 
-const emitter = new EventEmitter();
-emitter.on("beforeCommand", (instruction) => {
-    console.log('You wrote: ' + instruction + ', trying to run command');
-});
-emitter.on("afterCommand",  () => {
-    console.log('Finished command');
-});
+// fs.stat('./cat.jpg', (err, stats) => {
+//   const statMode = new StatMode(stats);
+//   onsole.log(statMode.toString());
+// });
 
-process.stdin.setEncoding('utf-8');
+// fs.readFile('./tekst.txt', 'utf-8', (err, data) => {
+//   console.log('Dane przed zapisem!'.blue);
+//   console.log(data);
+//   fs.appendFile('./tekst.txt', '\n\nA tak wyglądają po zapisie!', (err) => {
+//     if (err) throw err;
+//     console.log('Zapisano!'.blue);
+//     fs.readFile('./tekst.txt', 'utf-8', (err, data) => {
+//       console.log('Dane po zapisie'.blue)
+//       console.log(data);
+//     });
+//   });
+// });
 
-process.stdin.on('readable', () => {
-  const input = process.stdin.read();
-  if(input !== null) {
-    const instruction = input.toString().trim();
-
-    emitter.emit('beforeCommand', instruction);
-    switch (instruction) {
-      case '/exit':
-        process.stdout.write('Quitting app!\n');
-        process.exit();
-        break;
-      case '/nodeVer':
-        process.stdout.write(`Node version: ${process.versions.node}`);
-        process.exit();
-        break;
-      case '/sysLang':
-        process.stdout.write(`System Language: ${process.env.LANG}`);
-        process.exit();
-        break;
-      case '/getOSinfo':
-        OSinfo.print();
-        break;
-      default:
-      process.stderr.write('Wrong instruction!\n');
-    }
-    emitter.emit('afterCommand'); 
-  }
+fs.readdir('./node_modules', 'utf-8', (err, data) => {
+  fs.writeFile('./npm_modules.txt', `npm modules: ${data}`, (err) => {
+    if (err) throw err;
+    console.log('Zapisano!'.blue);
+    fs.readFile('./npm_modules.txt', 'utf-8', (err, data) => {
+        console.log('Dane po zapisie'.blue)
+        console.log(data);
+    });
+  });
 });
--- modules/timeFormat.js
+++
@@ -1,9 +0,0 @@
-function timeFormat(seconds) {
-  const hours = (seconds/3600).toFixed();
-  const secondsLeft = (seconds % 3600);
-  const minutes = Math.floor((secondsLeft/60)).toFixed();
-  seconds = (secondsLeft % 60).toFixed();
-  return `${hours}h ${minutes}min ${seconds}sec`;
-} 
-
-exports.timeFormat = timeFormat;
