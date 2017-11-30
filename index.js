4bbaf4488c60f5b62f37eeef2137a66816ce4310
Karol <karol.gratus@gmail.com>
2017-11-30 13:25:48 +0100

    added emmiter

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const EventEmitter = require("events").EventEmitter;
 const OSinfo = require('./modules/OSinfo');
+
+const emitter = new EventEmitter();
+emitter.on("beforeCommand", (instruction) => {
+    console.log('You wrote: ' + instruction + ', trying to run command');
+});
+emitter.on("afterCommand",  () => {
+    console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 
@@ -6,6 +15,8 @@ process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if(input !== null) {
     const instruction = input.toString().trim();
+
+    emitter.emit('beforeCommand', instruction);
     switch (instruction) {
       case '/exit':
         process.stdout.write('Quitting app!\n');
@@ -24,6 +35,7 @@ process.stdin.on('readable', () => {
         break;
       default:
       process.stderr.write('Wrong instruction!\n');
-    } 
+    }
+    emitter.emit('afterCommand'); 
   }
 });
