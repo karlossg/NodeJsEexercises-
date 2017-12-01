c6b203e2497fcd0647bdb60292ffe3797617f836
Karol <karol.gratus@gmail.com>
2017-12-01 10:09:19 +0100

    add new line to print data in file

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const fs = require('fs');
 const colors = require('colors');
-// const StatMode = require('stat-mode');
 
-// fs.stat('./cat.jpg', (err, stats) => {
-//   const statMode = new StatMode(stats);
-//   onsole.log(statMode.toString());
-// });
-
-// fs.readFile('./tekst.txt', 'utf-8', (err, data) => {
-//   console.log('Dane przed zapisem!'.blue);
-//   console.log(data);
-//   fs.appendFile('./tekst.txt', '\n\nA tak wyglądają po zapisie!', (err) => {
-//     if (err) throw err;
-//     console.log('Zapisano!'.blue);
-//     fs.readFile('./tekst.txt', 'utf-8', (err, data) => {
-//       console.log('Dane po zapisie'.blue)
-//       console.log(data);
-//     });
-//   });
-// });
 
 fs.readdir('./node_modules', 'utf-8', (err, data) => {
+  for (i = 0; i < data.length; i++) {
+    data[i] = '\n' + (i+1) + ') ' +  data[i];
+  }
   fs.writeFile('./npm_modules.txt', `npm modules: ${data}`, (err) => {
     if (err) throw err;
     console.log('Zapisano!'.blue);
     fs.readFile('./npm_modules.txt', 'utf-8', (err, data) => {
         console.log('Dane po zapisie'.blue)
-        console.log(data);
     });
   });
 });
